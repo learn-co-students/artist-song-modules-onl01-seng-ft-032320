3c28a89157579ec8d2e3f94fef49e030f336eb9b
Javante Nelson <javantenelson@gmail.com>
2020-04-08 08:39:53 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,42 +1,49 @@
-require 'pry'
+class Artist 
+  extend Memorable::ClassMethods
+end 
 
-class Artist
-  attr_accessor :name
-  attr_reader :songs
 
-  @@artists = []
 
-  def initialize
-    @@artists << self
-    @songs = []
-  end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+# require 'pry'
 
-  def self.all
-    @@artists
-  end
+# class Artist
+#   attr_accessor :name
+#   attr_reader :songs
 
-  def self.reset_all
-    self.all.clear
-  end
+#   @@artists = []
 
-  def self.count
-    self.all.count
-  end
+#   def initialize
+#     @@artists << self
+#     @songs = []
+#   end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
+#   def self.find_by_name(name)
+#     @@artists.detect{|a| a.name == name}
+#   end
 
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
+#   def self.all
+#     @@artists
+#   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-end
+#   def self.reset_all
+#     self.all.clear
+#   end
+
+#   def self.count
+#     self.all.count
+#   end
+
+#   def add_song(song)
+#     @songs << song
+#     song.artist = self
+#   end
+
+#   def add_songs(songs)
+#     songs.each { |song| add_song(song) }
+#   end
+
+#   def to_param
+#     name.downcase.gsub(' ', '-')
+#   end
+# end
--- lib/song.rb
+++ lib/song.rb
@@ -1,36 +1,44 @@
+class Song 
+  extend Memorable::ClassMethods
+end 
+
+
+
+
+
 require 'pry'
 
-class Song
-  attr_accessor :name
-  attr_reader :artist
+# class Song
+#   attr_accessor :name
+#   attr_reader :artist
 
-  @@songs = []
+#   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+#   def initialize
+#     @@songs << self
+#   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+#   def self.find_by_name(name)
+#     @@songs.detect{|a| a.name == name}
+#   end
 
-  def self.all
-    @@songs
-  end
+#   def self.all
+#     @@songs
+#   end
 
-  def self.reset_all
-    self.all.clear
-  end
+#   def self.reset_all
+#     self.all.clear
+#   end
 
-  def self.count
-    self.all.count
-  end
+#   def self.count
+#     self.all.count
+#   end
 
-  def artist=(artist)
-    @artist = artist
-  end
+#   def artist=(artist)
+#     @artist = artist
+#   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-end
+#   def to_param
+#     name.downcase.gsub(' ', '-')
+#   end
+# end
